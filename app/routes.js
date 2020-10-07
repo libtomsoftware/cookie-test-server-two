4b6e136c6f97d9ad3c94c9d4e848cacff0f4a759
Tomasz Libich <t.libich@gmail.com>
2020-10-07 18:09:26 +0100

    Remove iframe

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,6 @@ module.exports = function routes() {
 
   routes.get("/healthcheck", healthcheck.get);
   routes.get("/", function (req, res) {
-    res.headers = { ...res.headers, ...{ "X-Frame-Options": "Allow" } };
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   routes.all("*", (req, res) => responder.rejectNotFound(res));
